5e92140cb890196896f7c21f35b5481ec7178a29
Afonso <afonsorafael@sapo.pt>
2017-11-19 08:40:04 +0000

    update site

---
+++ thejava.js
@@ -0,0 +1,11 @@
+var data = [];
+
+function setup() {
+  createCanvas(outerWidth, outerHeight);
+  background(255, 0, 200);
+}
+
+//function mousePressed() {
+    //var x = Map(mouseX, 0, with, 0, 1);
+    //var y = Map(mouseY, 0, ,)
+//}
