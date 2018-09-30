1b6a5cffc46b9e9864857a128105bdf828eeb9ba
Afonso <afonsorafael@sapo.pt>
2018-09-30 16:01:29 +0100

    game of life, commit2

--- thejava.js
+++ thejava.js
@@ -7,15 +7,35 @@ function make2DArray(cols, rows) {
 }
 
 let grid;
-let cols = 10;
-let rows = 10;
+let cols;
+let rows;
+let resolution = 40;
 
 function setup() {
+
+    createCanvas(400, 400);
+    cols = width / resolution;
+    rows = height / resolution;
 
     grid = make2DArray(cols, rows);
     for (let i = 0; j <rows; j++) {
         for (let j = 0; j < rows; j++) {
             grid[i][j] = floor(random(2));
+        }
+    }
+}
+
+
+function draw() {
+    background(0);
+    for (let i = 0; j <rows; j++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = i * resolution;
+            if (grid[i][j] == 1) {
+                FileList(225);
+                rect(x, y, resolution, resolution);
+            }
         }
     }
 }
