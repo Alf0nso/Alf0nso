3f77103a86940a97a72d345ec8d77dce6f6bd116
Afonso <afonsorafael@sapo.pt>
2018-10-02 00:23:37 +0100

    stable config

--- thejava.js
+++ thejava.js
@@ -13,16 +13,63 @@ let resolution = 10;
 
 function setup() {
 
-    createCanvas(600, 400);
+    createCanvas(1000, 700);
+    frameRate(20);
     cols = width / resolution;
     rows = height / resolution;
 
     grid = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            grid[i][j] = floor(random(2));
+            grid[i][j] = 0;
         }
     }
+    //the block
+    grid[30][20] = 1;
+    grid[30][21] = 1;
+    grid[31][20] = 1;
+    grid[31][21] = 1;
+
+    
+    grid[40][20] = 1;
+    grid[40][21] = 1;
+    grid[40][22] = 1;
+    //3
+    grid[41][23] = 1;
+    grid[42][24] = 1;
+    grid[43][24] = 1;
+
+
+    grid[45][23] = 1;
+    grid[46][22] = 1;
+    //3
+    grid[44][21] = 1;
+    grid[46][21] = 1;
+    grid[47][21] = 1;
+
+    grid[46][20] = 1;
+    grid[45][19] = 1;
+    grid[43][18] = 1;
+    grid[42][18] = 1;
+    grid[41][19] = 1;
+
+    grid[50][20] = 1;
+    grid[50][19] = 1;
+    grid[50][18] = 1;
+    grid[51][20] = 1;
+    grid[51][19] = 1;
+    grid[51][18] = 1;
+
+    grid[52][17] = 1;
+    grid[52][21] = 1;
+    grid[54][17] = 1;
+    grid[54][21] = 1;
+    grid[54][16] = 1;
+    grid[54][22] = 1;
+    grid[64][19] = 1;
+    grid[64][18] = 1;
+    grid[65][19] = 1;
+    grid[65][18] = 1;
 }
 
 
